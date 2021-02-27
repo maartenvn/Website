29c5bfd2f1d405041d4a99730982e93689eb33b5
Maarten Van Neyghem <maartenvn.eu@gmail.com>
2021-02-27 10:08:08 +0100

    fix: typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -118,7 +118,7 @@ export default {
             {
                 hid: "og:title",
                 property: "og:title",
-                content: "Maarten Van Neygem"
+                content: "Maarten Van Neyghem"
             },
             {
                 hid: "og:description",
